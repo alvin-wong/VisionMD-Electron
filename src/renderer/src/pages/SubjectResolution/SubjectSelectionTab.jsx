26817791519a4c379b30f4f707f5c1ee7c348c0f
alvin-wong <alvinwong@ufl.edu>
2025-07-29 14:31:27 -0400

    Fix subject selection tab colors

--- src/renderer/src/pages/SubjectResolution/SubjectSelectionTab.jsx
+++ src/renderer/src/pages/SubjectResolution/SubjectSelectionTab.jsx
@@ -6,23 +6,25 @@ import JSONUploadDialog from './SubjectJSONUploadDialog';
 
 const PersonRow = ({ person, onPlay, onToggleSubject }) => (
   <li
-    className={`flex items-center justify-between p-2 border bg-transparent border-zinc-600 rounded-lg transition-colors duration-150 ${
-      person.isSubject ? 'hover:bg-zinc-700 text-white' : 'bg-gray-100 hover:bg-gray-200'
+    className={`flex items-center justify-between p-2 border bg-transparent border-zinc-600 rounded-lg transition-colors duration-150${
+      person.isSubject ? 'hover:bg-zinc-700 text-gray-100' : 'hover:bg-zinc-700 text-gray-100'
     }`}
   >
-    <div className="flex items-center">
-      <PlayCircleOutline onClick={() => onPlay(person.timestamp)} className="cursor-pointer" />
-      <span className="ml-2 font-medium">{person.name}</span>
-      <span className="ml-4 text-sm">{person.timestamp}</span>
+    <div className='flex flex-row justify-between w-full'>
+      <div className="flex items-center">
+        <PlayCircleOutline onClick={() => onPlay(person.timestamp)} className="cursor-pointer" />
+        <span className="ml-2 font-medium">{person.name}</span>
+        <span className="ml-4 text-sm">{person.timestamp}</span>
+      </div>
+      <button
+        onClick={() => onToggleSubject(person)}
+        className={`px-4 py-1 rounded text-xs font-semibold shadow-sm transition-colors duration-150 ${
+          person.isSubject ? 'bg-red-500 hover:bg-red-600 text-gray-100' : 'bg-emerald-600 hover:bg-emerald-700 text-gray-100'
+        }`}
+      >
+        {person.isSubject ? 'Remove as Subject' : 'Mark as Subject'}
+      </button>
     </div>
-    <button
-      onClick={() => onToggleSubject(person)}
-      className={`px-4 py-1 rounded text-xs font-semibold shadow-sm transition-colors duration-150 ${
-        person.isSubject ? 'bg-red-500 hover:bg-red-600 text-white' : 'bg-emerald-600 hover:bg-emerald-700 text-white'
-      }`}
-    >
-      {person.isSubject ? 'Remove as Subject' : 'Mark as Subject'}
-    </button>
   </li>
 );
 
@@ -163,7 +165,7 @@ const SubjectSelectionTab = ({
             <div className="flex flex-col border-b-2 py-2 text-gray-100 border-zinc-500">
               Subjects
             </div>
-            <ul className="py-2">
+            <ul className="py-2 space-y-2">
               {persons.map((subject, index) => (
                 <PersonRow
                   key={subject.id || index}
